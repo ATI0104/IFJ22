cfcf541c048b6e11f9ea583cf634cffbd4a23fe1
Anna02-08 <94108386+Anna02-08@users.noreply.github.com>
2022-10-17 19:18:05 +0200

    Update symtable.c

--- symtable.c
+++ symtable.c
@@ -1,62 +1,62 @@
 #include "symtable.h"
-//TODO check for memory leaks
-void symtable_init(symtable **tree) {
+
+void function_table_init(function_table **tree) {
   *tree = NULL;
   return;
 }
-void symtable_add(symtable **tree, symtable *leaf) {
+void function_table_add(function_table **tree, function_table *leaf) {
   if (*tree == NULL) {
     *tree = leaf;
     return;
   }
   if (strcmp((*tree)->name.txt, leaf->name.txt) > 0) {
-    symtable_add(&(*tree)->right, leaf);
+    function_table_add(&(*tree)->right_func, leaf);
   } else
-    symtable_add(&(*tree)->left, leaf);
+    function_table_add(&(*tree)->left_func, leaf);
 }
-symtable *symtable_get(symtable **tree, string name) {
+function_table *function_table_get(function_table **tree, string name) {
   if (*tree == NULL) return NULL;
   if (strcmp((*tree)->name.txt, name.txt) == 0) return *tree;
   if (strcmp((*tree)->name.txt, name.txt) > 0) {
-    return symtable_get(&(*tree)->right, name);
+    return function_table_get(&(*tree)->right_func, name);
   } else
-    return symtable_get(&(*tree)->left, name);
+    return function_table_get(&(*tree)->left_func, name);
 }
-void symtable_destroy(symtable **tree) {
+void function_table_destroy(function_table **tree) {
   if (*tree == NULL) return;
-  symtable_destroy(&((*tree)->left));
-  symtable_destroy(&((*tree)->right));
+  function_table_destroy(&((*tree)->left_func));
+  function_table_destroy(&((*tree)->right_func));
   free((*tree)->name.txt);
   free(*tree);
   *tree = NULL;
 }
-void symtable_delete(symtable **tree, string name) {
+void function_table_delete(function_table **tree, string name) {
   if (*tree == NULL) return;
   if (strcmp((*tree)->name.txt, name.txt) < 0) {
-    symtable_delete(&(*tree)->left, name);
+    function_table_delete(&(*tree)->left_func, name);
     return;
   } else if (strcmp((*tree)->name.txt, name.txt) > 0) {
-    symtable_delete(&(*tree)->right, name);
+    function_table_delete(&(*tree)->right_func, name);
     return;
   } else {
-    if ((*tree)->left == NULL && (*tree)->right == NULL) {
+    if ((*tree)->left_func == NULL && (*tree)->right_func == NULL) {
       free(*tree);
       *tree = NULL;
     } else {
-      if ((*tree)->left != NULL && (*tree)->right != NULL) {
-        if ((*tree)->left->right != NULL) {
-          symtable_replace_by_rightmost(*tree, &((*tree)->left));
+      if ((*tree)->left_func != NULL && (*tree)->right_func != NULL) {
+        if ((*tree)->left_func->right_func != NULL) {
+          func_table_replace_by_rightmost(*tree, &((*tree)->left_func));
         }
       } else {
-        if ((*tree)->left == NULL) {
-          symtable *tmp = *tree;
-          *tree = (*tree)->right;
+        if ((*tree)->left_func == NULL) {
+          function_table *tmp = *tree;
+          *tree = (*tree)->right_func;
           free(tmp->name.txt);
           free(tmp);
         }
-        if ((*tree)->right == NULL) {
-          symtable *tmp = *tree;
-          *tree = (*tree)->left;
+        if ((*tree)->right_func == NULL) {
+          function_table *tmp = *tree;
+          *tree = (*tree)->left_func;
           free(tmp->name.txt);
           free(tmp);
         }
@@ -65,15 +65,15 @@ void symtable_delete(symtable **tree, string name) {
   }
 }
 
-void symtable_replace_by_rightmost(symtable *target, symtable **tree) {
-  if ((*tree)->right == NULL) {
+void func_table_replace_by_rightmost(function_table *target, function_table **tree) {
+  if ((*tree)->right_func == NULL) {
     target->type = (*tree)->type;
     target->name = (*tree)->name;
-    target->i_value = (*tree)->i_value;
-    target->f_value = (*tree)->f_value;
-    target->null = (*tree)->null;
-    symtable_delete(tree, target->name);
+    target->input_type = (*tree)->input_type;
+    target->output_type = (*tree)->output_type;
+    target->variable = (*tree)->variable;
+    function_table_delete(tree, target->name);
   } else
-    symtable_replace_by_rightmost(target, &(*tree)->right);
+    func_table_replace_by_rightmost(target, &(*tree)->right_func);
 }
-symtable *allocate_leaf() { return malloc(sizeof(symtable)); }
+function_table *allocate_leaf() { return malloc(sizeof(function_table)); }
