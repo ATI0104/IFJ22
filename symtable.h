647bfcadbbaec361227eb9ae5bdaf4822e0c5a07
ATI0104 <ATI0104@users.noreply.github.com>
2022-10-19 00:19:59 +0200

    Fix: multiple header definitions

--- symtable.h
+++ symtable.h
@@ -2,29 +2,31 @@
 
 #include "string.h"
 
-typedef struct input_param_list{
+#ifndef _symtable
+#define _symtable
+
+typedef struct input_param_list {
   int type;
   struct input_param_list *next;
-  }input_param_list;
+} input_param_list;
 
-typedef struct var_table{
-  int type; //Declared type
+typedef struct var_table {
+  int type;  // Declared type
   string name;
   int num;
   double fl;
   string s;
   struct var_table *left_var;
   struct var_table *right_var;
-}var_table;
-
+} var_table;
 
 typedef struct function_table {
   string name;
 
-  //input param
- input_param_list input_type;
+  // input param
+  input_param_list input_type;
 
-  //output type
+  // output type
   int output_type;
 
   var_table variable;
@@ -32,13 +34,14 @@ typedef struct function_table {
   struct function_table *left_func;
   struct function_table *right_func;
 
-}function_table;
-
+} function_table;
 
 void function_table_init(function_table **tree);
 void function_table_add(function_table **tree, function_table *leaf);
 function_table *function_table_get(function_table **tree, string name);
 void function_table_destroy(function_table **tree);
 void function_table_delete(function_table **tree, string name);
-void func_table_replace_by_rightmost(function_table *target, function_table **tree);
+void func_table_replace_by_rightmost(function_table *target,
+                                     function_table **tree);
 function_table *allocate_leaf();
+#endif
