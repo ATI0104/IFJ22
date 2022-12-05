804a157c774c0ad550d5c4399c857d798d77a413
ATI0104 <ATI0104@users.noreply.github.com>
2022-12-06 00:30:59 +0100

    Saving these Just in case my laptop blows up or something

--- generator.c
+++ generator.c
@@ -23,32 +23,110 @@ void generate_code(code* c, function_table* f, string* name) {
   } else {
     printf("DPRINT string@main");
   }
-  if (c->i) {
-    generate_if(c->i, c->e, c->expression, f, name);
-  } else if (c->loop) {
-    generate_while(c->loop, c->expression, f, name);
-  } else if (c->var) {
-    generate_assignment(c->var, c->expression, f, name, v);
-  } else if (c->ret) {
-    generate_return(expression, f, name);
-  } else if (c->expression) {
-    generate_expression(c->expression, f, name, v);
-  } else if (c->func) {
-    generate_call(c->func, f, name, v);
+  while (c != NULL) {
+    if (c->i) {
+      generate_if(c->i, c->e, c->expression, f, name);
+    } else if (c->loop) {
+      generate_while(c->loop, c->expression, f, name);
+    } else if (c->var) {
+      generate_assignment(c->var, c->expression, f, name, &v);
+    } else if (c->ret) {
+      generate_return(c->expression, f, name, v);
+    } else if (c->expression) {
+      generate_expression(c->expression, f, name, v);
+    } else if (c->jmp) {
+      generate_call(c->jmp, f, name, v);
+    }
+    c = c->next;
   }
 }
 
-void generate_if(code* ifpart, code* elsepart, expression* e, function_table* f,
+void generate_if(code* ifpart, code* elsepart, expr* e, function_table* f,
                  string* functionname) {}
 
-void generate_while(code* loop, expression* e, function_table* f,
+void generate_while(code* loop, expr* e, function_table* f,
                     string* functionname) {}
 
-void generate_assignment(variable* var, expression* e, function_table* f,
-                         string* functionname, varlist* v) {}
+void generate_assignment(string* var, expr* e, function_table* f,
+                         string* functionname, varlist** v) {
+  printf("DEFVAR LF@%s", var->txt);
+  varlist_set(v, var, get_expression_type(e, *v, f));
+  evaluate_expression(e, f, functionname, *v, var);
+}
 
-void generate_return(expression* e, function_table* f, string* functionname,
+void generate_return(expr* e, function_table* f, string* functionname,
                      varlist* v) {}
 
-void evaluate_expression(expression* e, function_table* f, string* functionname,
-                         varlist* v, string* tovar) {}
+void evaluate_expression(expr* e, function_table* f, string* functionname,
+                         varlist* v, string* tovar) {
+  Stack* s;
+  Stack_Init(&s);
+  while (e != NULL) {
+    if (e->num) {
+      printf("PUSHS int@%d\n", *(e->num));
+      Stack_Push(&s, _int);
+    } else if (e->str) {
+      printf("PUSHS string@%s, e->str->txt");
+      Stack_Push(&s, _string);
+    } else if (e->fl) {
+      printf("PUSHS float@%a", *(e->fl));
+      Stack_Push(&s, _float);
+    } else if (e->var) {
+      int type = varlist_get(v, e->var);
+      switch (type) {
+        case _int:
+          printf("PUSHS LF@%s\n", e->var->txt);
+          Stack_Push(&s, _int);
+          break;
+        case _float:
+          printf("PUSHS LF@%s\n", e->var->txt);
+          Stack_Push(&s, _float);
+          break;
+        case _string:
+          printf("PUSHS LF@%s\n", e->var->txt);
+          Stack_Push(&s, _string);
+          break;
+        case _void:
+          printf("PUSHS LF@%s\n", e->var->txt);
+          Stack_Push(&s, _null);
+          break;
+        case _bool:
+          printf("PUSHS LF@%s\n", e->var->txt);
+          Stack_Push(&s, _bool);
+          break;
+        default:
+          break;
+      }
+    } else if (e->op) {  // TODO add all operators & verify that both of the
+                         // operands are of the same type if not convert them
+      switch (*(e->op)) {
+        case _plus:
+          Stack_Pop(s);
+          Stack_Pop(s);
+          printf("ADDS\n");
+        case _minus:
+          Stack_Pop(s);
+          Stack_Pop(s);
+          printf("SUBS\n");
+          break;
+        case _multiply:
+          Stack_Pop(s);
+          Stack_Pop(s);
+          printf("MULS\n");
+          break;
+        case _divide:
+          Stack_Pop(s);
+          Stack_Pop(s);
+          printf("DIVS\n");
+          break;
+        default:
+          break;
+      }
+    }
+    e = e->next;
+  }
+  if (!Stack_IsEmpty(s)) {
+    Stack_Pop(s);
+    printf("POPS LF@%s\n", tovar->txt);
+  }
+}
--- generator.h
+++ generator.h
@@ -6,5 +6,14 @@
 #include "semantics.h"
 #include "string.h"
 #include "symtable.h"
-
+void evaluate_expression(expr* e, function_table* f, string* functionname,
+                         varlist* v, string* tovar);
+void generate_return(expr* e, function_table* f, string* functionname,
+                     varlist* v);
+void generate_assignment(string* var, expr* e, function_table* f,
+                         string* functionname, varlist** v);
+void generate_while(code* loop, expr* e, function_table* f,
+                    string* functionname);
+void generate_if(code* ifpart, code* elsepart, expr* e, function_table* f,
+                 string* functionname);
 #endif
