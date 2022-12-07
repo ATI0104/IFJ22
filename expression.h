e662880e36f56268855adde3fe5c90e66dbc0187
Anna02-08 <94108386+Anna02-08@users.noreply.github.com>
2022-12-07 15:41:25 +0100

    Update expression.h

--- expression.h
+++ expression.h
@@ -14,7 +14,7 @@ typedef enum {
   EXPR_REL,
   EXPR_L_BRACKET,
   EXPR_R_BRACKET,
-  EXPR_ID,  // Identifier, string, number ...
+  EXPR_ID,  // Variable, string, float, integer
   EXPR_DOLLAR,
   EXPR_STRLEN,
   EXPR_NON_TERMINAL,
@@ -25,7 +25,7 @@ typedef enum {
 typedef enum {
   S,  // shift
   R,  // reduce
-  Q,  // equals (for brackets)
+  E,  // equals 
   N   // error
 } precendencyOperation;
 
@@ -41,67 +41,22 @@ typedef struct stack {
   int totalAmount;
 } TStack;
 
-/**
- * @brief Initialize stack
- *
- * @param stack Stack to initialize
- */
 
-void stackInit(TStack *stack);
-
-/**
- * @brief Pushes an element to the stack
- *
- * @param stack Stack to push to
- * @param input Value to push
- * @param tok Current token to store
- */
-void stackPush(TStack *stack, precedencyInput input, expr *exp);
-
-/**
- * @brief Pops an element from stack
- *
- * @param stack Stack to pop from
- */
-
-void stackPop(TStack *stack);
-
-/**
- * @brief Inserts handle in front of the first terminal it finds
- *
- * @param stack Stack to insert to
- */
-void stackInsertHandle(TStack *stack);
-
-/**
- * @brief Returns the first terminal that occurs in the stack
- *
- * @param stack Stack where terminal should be found
- * @return Value of the first terminal that is found on the stack
- */
-precedencyInput stackTopTerminal(TStack *stack);
-
-/**
- * @brief Removes the nearest item from stack
- *
- * @param stack Stack to remove handle from
- * @param item Item to delete
- */
-void deleteNearestItem(TStack *stack, precedencyInput item);
-
-/**
- * @brief Removes all of the items from stack
- *
- * @param stack Stack to remove items from
- */
-void stackDispose(TStack *stack);
-
+precedencyInput GetExpressionType(expr *exp);
 precendencyOperation getOperationFromTable(precedencyInput onStack, precedencyInput onInput);
-precedencyInput convertTokenTypeToExpressionType(expr *exp);
-bool endOfExpression(expr *exp);
 void insertAfter(expr *exp);
 bool reduceDyadicOperation(TStack *stack, StackItem **possibleRule);
 bool reduceIdentifier(TStack *stack, StackItem **possibleRule);
 bool reduce(TStack *stack);
 bool parseExpression(expr *exp);
+
+
+/****EXPRESSION STACK OPERATIONS ***/
+void stackInit(TStack *stack);
+void stackPush(TStack *stack, precedencyInput input, expr *exp);
+void stackPop(TStack *stack);
+void stackInsertHandle(TStack *stack);
+precedencyInput stackTopTerminal(TStack *stack);
+void deleteNearestItem(TStack *stack, precedencyInput item);
+void stackDispose(TStack *stack);
 #endif
