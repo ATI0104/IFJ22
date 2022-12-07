91e4a0eccc0f7c5e64d923f65952daf61de96d5d
Anna02-08 <94108386+Anna02-08@users.noreply.github.com>
2022-12-07 22:22:31 +0100

    Update expression.h

--- expression.h
+++ expression.h
@@ -46,22 +46,63 @@ typedef struct stack {
   int totalAmount;
 } TStack;
 
-
+/**
+ * @brief Returns expression type
+ */
 precedencyInput GetExpressionType(expr *exp);
+/**
+ * @brief Get the operation from precedency table
+ */ 
 precendencyOperation getOperationFromTable(precedencyInput onStack, precedencyInput onInput);
+/**
+ * @brief Inserts expression to the expression structure
+ */
 void insertAfter(expr *exp);
-bool reduceDyadicOperation(TStack *stack, StackItem **possibleRule);
+/**
+ * @brief Reduces operations (a+b, a*b, (a),...)
+ */
+bool reduceOperation(TStack *stack, StackItem **possibleRule);
+/**
+ * @brief Reduces the identifier (Variable, string, float, integer)
+ */
 bool reduceIdentifier(TStack *stack, StackItem **possibleRule);
+/**
+ * @brief Reduces the expression
+ */
 bool reduce(TStack *stack);
+/**
+ * @brief Parses current expression
+ */
 bool parseExpression(expr *exp);
 
 
 /****EXPRESSION STACK OPERATIONS ***/
+/*
+ * @brief Initialize stack
+ */
 void stackInit(TStack *stack);
+/*
+ * @brief Pushes an element to the stack
+ */
 void stackPush(TStack *stack, precedencyInput input, expr *exp);
+/*
+ * @brief Pops an element from stack
+ */
 void stackPop(TStack *stack);
+/*
+ * @brief Inserts handle in front of the first terminal it finds
+ */
 void stackInsertHandle(TStack *stack);
+/*
+ * @brief Returns the first terminal that occurs in the stack
+ */
 precedencyInput stackTopTerminal(TStack *stack);
+/*
+ * @brief Removes the nearest item from stack
+ */
 void deleteNearestItem(TStack *stack, precedencyInput item);
+/*
+ * @brief Removes all of the items from stack
+ */
 void stackDispose(TStack *stack);
 #endif
