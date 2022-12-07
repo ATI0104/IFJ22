aef6690a0524b6c39e864dbeb8e332de4ffff52a
Anna02-08 <94108386+Anna02-08@users.noreply.github.com>
2022-12-07 22:44:02 +0100

    Update stack.h

--- stack.h
+++ stack.h
@@ -14,11 +14,32 @@ typedef struct opstack {
   int topIndex;
   int size;
 } Stack;
+/**
+* @brief Initializes stack
+*/
 void Stack_Init(Stack**);
+/**
+* @brief Checks if stack is empty
+*/
 int Stack_IsEmpty(const Stack*);
+/**
+* @brief Checks if stack is full
+*/
 int Stack_IsFull(const Stack*);
+/**
+* @brief Gets the top pointer of the stack
+*/
 void Stack_Top(const Stack*, int*);
+/**
+ * Pops a pointer from the stack.
+ */
 void Stack_Pop(Stack*);
+/**
+ * Adds a data to the stack.
+ */
 void Stack_Push(Stack**, int);
+/**
+ * Removes the entire stack
+ */
 void Stack_Destroy(Stack** stack);
 #endif
